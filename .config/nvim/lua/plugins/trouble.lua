9609aa1a9fbccd96009fdc47096202fe4adad8e1
luytbq <luytbq43@gmail.com>
2025-04-25 20:45:23 +0700

    idk

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -29,7 +29,7 @@ return {
       desc = "Location List (Trouble)",
     },
     {
-      "<leader>xQ",
+      "<leader>xo",
       "<cmd>Trouble qflist toggle<cr>",
       desc = "Quickfix List (Trouble)",
     },
