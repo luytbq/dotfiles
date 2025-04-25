5ce196abbf8bb544e4bd5de73c3249591350539e
LuytBui <luytbq43@gmail.com>
2025-04-25 18:20:18 +0700

    nvim create user command ChangeTabWidth

---
+++ .config/nvim/lua/config/usercmds.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_create_user_command("ChangeTabWidth",
+    function(cmd_args)
+        local tabWidth = cmd_args.args or 4
+        vim.cmd("set tabstop=" .. tabWidth)
+        vim.cmd("set softtabstop=" .. tabWidth)
+        vim.cmd("set shiftwidth=" .. tabWidth)
+    end,
+    {
+        nargs=1 -- see :h command-nargs
+    }
+)
+
