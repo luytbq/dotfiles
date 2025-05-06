08e23396c606e8f5fa14039e4b6ab3209d2afa4a
luytbq <luytbq43@gmail.com>
2025-05-06 21:20:10 +0700

    disable <c-j> and <c-k> in snack picker explorer

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,9 +3,13 @@ return {
   ---@type snacks.Config
   opts = {
     terminal = {
-      -- your terminal configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     },
+    picker = {
+ 
+    },
+    keys = {
+        {"<c-j>", false},
+        {"<c-k>", false}
+    }
   },
 }
