75dea0a48794ec80994097ef559f73e6f34abcbf
luytbq <luytbq43@gmail.com>
2025-05-21 00:51:50 +0700

    [cherry-picked] autocmd remove trailing spaces before write buf

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -14,3 +14,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- before writing the buffer to file, remove all trailing spaces
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    local save_cursor = vim.api.nvim_win_get_cursor(0)
+    vim.cmd([[%s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, save_cursor)
+  end,
+})
