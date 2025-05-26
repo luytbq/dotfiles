2b2b4bf376dc15e1c24b87856e6a29a9c8b75502
luytbq <luytbq43@gmail.com>
2025-05-26 08:42:50 +0700

    [cherry-picked] keymap FloatTerm

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -12,30 +12,35 @@ map("v", "<leader>x", ":lua<CR>")
 -- Quickfix navigation
 X_QUICKFIX_OPENING = false
 map("n", "<leader>qo", -- toggle the quickfix list window
-  function()
-  if not X_QUICKFIX_OPENING then
-    vim.cmd("copen")
-    X_QUICKFIX_OPENING = true
-  else
-    vim.cmd("cclose")
-    X_QUICKFIX_OPENING = false
-  end
-end)
+	function()
+		if not X_QUICKFIX_OPENING then
+			vim.cmd("copen")
+			X_QUICKFIX_OPENING = true
+		else
+			vim.cmd("cclose")
+			X_QUICKFIX_OPENING = false
+		end
+	end)
 map("n", "[q", "<cmd>cprev<CR>")
 map("n", "]q", "<cmd>cnext<CR>")
 
 -- Change paste behavior: keep register after overwrite text
 map({ "v" }, "p", "pgvy")
 
-map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
-map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+map("n", "<A-j>", ":m .+1<CR>==")     -- move line up(n)
+map("n", "<A-k>", ":m .-2<CR>==")     -- move line down(n)
 map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 
 -- Tab navigation
-map({"n", "v"}, "[t", "<cmd>tabprev<CR>")
-map({"n", "v"}, "]t", "<cmd>tabnext<CR>")
+map({ "n", "v" }, "[t", "<cmd>tabprev<CR>")
+map({ "n", "v" }, "]t", "<cmd>tabnext<CR>")
 
+map({ "i", "t" }, "<esc><esc>", "<c-\\><c-n>")
+
+map({ "n", "i", "v" }, "<leader>ft", "<cmd>FloatTerm<cr>", { desc = "Toggle Floating Terminal" })
+map({ "n", "i", "v" }, "<c-/>", "<cmd>FloatTerm<cr>", { desc = "Toggle Floating Terminal" })
+map({ "n", "i", "v" }, "<c-_>", "<cmd>FloatTerm<cr>", { desc = "Toggle Floating Terminal" })
 
 require("config/usercmds")
 require("config/luasnip")
