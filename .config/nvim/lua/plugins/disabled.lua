4fc02a8b4ce6db8747850845b0f616e88cff8902
luytbq <luybbq@onepay.vn>
2025-06-17 08:41:37 +0700

    disabled.lua

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,10 @@
 return {
+	{ "yetone/avante.nvim",      enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false },
+	-- { "stevearc/conform.nvim",   enabled = false },
+	-- { "stevearc/conform.nvim",   enabled = false },
+	-- { "gelguy/wilder.nvim",      enabled = false },
+	-- { "mfussenegger/nvim-dap",   enabled = false },
+	-- { "leoluz/nvim-dap-go",      enabled = false },
+	-- { "folke/snacks.nvim",       enabled = false },
 }
