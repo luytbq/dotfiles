fac2aeefdf53fb1bbe1a4511955e364ea9b3b4ee
LuytBui <luytbq43@gmail.com>
2025-07-18 11:32:11 +0700

    change tabs config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -9,58 +9,26 @@
 
 -- disable auto format
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "*.java", "*.ts", "*.js", "*.vue", "*.html" },
-	callback = function()
-		vim.b.autoformat = false
-	end,
+    pattern = { "*.java", "*.ts", "*.js", "*.vue", "*.html" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
 })
 
 -- before writing buffer, remove all trailing spaces
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		local save_cursor = vim.api.nvim_win_get_cursor(0)
-		vim.cmd([[%s/\s\+$//e]])
-		vim.api.nvim_win_set_cursor(0, save_cursor)
-	end,
+    pattern = "*.go",
+    callback = function()
+        local save_cursor = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[%s/\s\+$//e]])
+        vim.api.nvim_win_set_cursor(0, save_cursor)
+    end,
 })
 
 -- before writing buffer with specified pattern, re-indent it
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = { "*.go", "*.lua" },
-	callback = function()
-		vim.lsp.buf.format()
-	end,
-})
-
-vim.api.nvim_create_user_command("EditorConfig",
-	function()
-		-- Find .editorconfig in cwd or parent
-		local editorconfig_path = vim.fn.findfile(".editorconfig", ".;")
-		if editorconfig_path == "" then
-			return
-		end
-
-		-- Run sed to update or add the setting
-		-- Replace line if exists
-		local sed_cmd = string.format([[
-			if grep -q '^\s*trim_trailing_whitespace\s*=' %s; then
-				sed -i 's/^\s*trim_trailing_whitespace\s*=.*/trim_trailing_whitespace = false/' %s
-			fi
-		]], editorconfig_path, editorconfig_path)
-
-		vim.fn.system({ "bash", "-c", sed_cmd })
-	end,
-	{ nargs = 0 }
-)
--- overwrite .editorconfig: trim_trailing_whitespace = false
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		local git_remote_url = vim.fn.system("git remote get-url origin")
-		if not git_remote_url:match("onepay") then
-			return
-		end
-
-		vim.cmd("EditorConfig")
-	end,
+    pattern = { "*.go", "*.lua" },
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
