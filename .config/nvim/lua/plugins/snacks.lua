afc68484a47ed9deb56f2b2f0c203f83bfdca163
LuytBui <luytbq43@gmail.com>
2025-09-29 16:49:35 +0700

    switch from telescope to snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,22 +1,27 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    picker = {
-      win = {
-        input = {
-          keys = {
-            ["<c-j>"] = false,
-            ["<c-k>"] = false
-          }
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    keys = {
+        { "<leader>ff", function() Snacks.picker.smart() end, desc = "Smart Find Files", },
+        { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+        { "<leader>fi", function() Snacks.picker.grep() end, desc = "Grep" },
+    },
+    opts = {
+        picker = {
+            win = {
+                input = {
+                    keys = {
+                        ["<c-j>"] = false,
+                        ["<c-k>"] = false,
+                    },
+                },
+                list = {
+                    keys = {
+                        ["<c-j>"] = false,
+                        ["<c-k>"] = false,
+                    },
+                },
+            },
         },
-        list = {
-          keys = {
-            ["<c-j>"] = false,
-            ["<c-k>"] = false
-          }
-        }
-      },
-    }
-  },
+    },
 }
