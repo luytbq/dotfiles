85dc776a0be1499f87a2cfd2c48aceefab11b679
luytbq <luytbq43@gmail.com>
2025-10-23 23:37:15 +0700

    clean

--- .config/nvim/lua/plugins/lsp.lua
+++
@@ -1,9 +0,0 @@
-if true then
-	return {}
-end
-
-return {
-	{
-		"mason.nvim", { "mason-org/mason-lspconfig.nvim", config = function() end },
-	}
-}
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,7 @@ return {
         event = "LazyFile",
         dependencies = {
             "mason.nvim",
-            { "mason-org/mason-lspconfig.nvim", config = function() end },
+            { "mason-org/mason-lspconfig.nvim" },
         },
         opts = function()
             ---@class PluginLspOpts
@@ -68,7 +68,6 @@ return {
                     timeout_ms = nil,
                 },
                 -- LSP Server Settings
-                ---@alias lazyvim.lsp.Config vim.lsp.Config|{mason?:boolean, enabled?:boolean}
                 ---@type table<string, lazyvim.lsp.Config|boolean>
                 servers = {
                     lua_ls = {
@@ -222,50 +221,4 @@ return {
             end
         end),
     },
-
-    -- cmdline tools and lsp servers
-    {
-        "mason.nvim",
-        { "mason-org/mason-lspconfig.nvim", config = function() end },
-    },
-    -- {
-    --
-    --     "mason-org/mason.nvim",
-    --     cmd = "Mason",
-    --     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    --     build = ":MasonUpdate",
-    --     opts_extend = { "ensure_installed" },
-    --     opts = {
-    --         ensure_installed = {
-    --             "stylua",
-    --             "shfmt",
-    --             "java-debug-adapter",
-    --             "java-test",
-    --         },
-    --     },
-    --     ---@param opts MasonSettings | {ensure_installed: string[]}
-    --     config = function(_, opts)
-    --         require("mason").setup(opts)
-    --         local mr = require("mason-registry")
-    --         mr:on("package:install:success", function()
-    --             vim.defer_fn(function()
-    --                 -- trigger FileType event to possibly load this newly installed LSP server
-    --                 require("lazy.core.handler.event").trigger({
-    --                     event = "FileType",
-    --                     buf = vim.api.nvim_get_current_buf(),
-    --                 })
-    --             end, 100)
-    --         end)
-    --
-    --         mr.refresh(function()
-    --             for _, tool in ipairs(opts.ensure_installed) do
-    --                 local p = mr.get_package(tool)
-    --                 if not p:is_installed() then
-    --                     p:install()
-    --                 end
-    --             end
-    --         end)
-    --     end,
-    -- },
-
 }
