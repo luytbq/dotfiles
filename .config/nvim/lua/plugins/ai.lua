1f86cece7ef71a51793252b216c03f222e31fda1
Bùi Quốc Luýt <luytbq@onepay.vn>
2025-12-05 15:42:23 +0700

    [cherry-picked] nvim plugin: add mcphub plugin and refresh lockfile

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -171,5 +171,15 @@ return {
                 mappings = mapping,
             }
         end,
+    },
+    {
+        "ravitemer/mcphub.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+        config = function()
+            require("mcphub").setup()
+        end
     }
 }
