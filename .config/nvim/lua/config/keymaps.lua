c5773dd0c1820a77a8ea087618546168783c6eb4
Bùi Quốc Luýt <luytbq@onepay.vn>
2026-01-09 09:43:48 +0700

    nvim keymap change

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -49,6 +49,8 @@ map({ "n" }, "<leader>td", "<cmd>FloatTermDel<cr>", { desc = "Delete Current Flo
 map({ "v" }, "<c-/>", "<cmd>FloatTermVisual<cr>", { desc = "Send selected text to Floating Terminal" })
 map({ "v" }, "<c-_>", "<cmd>FloatTermVisual<cr>", { desc = "Send selected text to Floating Terminal" })
 
+map({ "n", "v" }, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", { desc = "LSP buffer code action" })
+
 -- map({ "n", "i", "v" }, "<c-x>", "<cmd>CopilotChatToggle<cr>", { desc = "CopilotChatToggle" })
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<c-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
