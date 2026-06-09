2144b72b8701817d49cb60464fcd04b8eff60af9
Bùi Quốc Luýt <luytbq@onepay.vn>
2026-06-09 09:39:57 +0700

    refactor(nvim): rename MarkdownRender command to ToggleMarkdownRender

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -4,10 +4,8 @@ return {
   opts = {},
   config = function(_, opts)
     require("render-markdown").setup(opts)
-    vim.api.nvim_create_user_command("MarkdownRender", function(args)
-      if args.args == "toggle" then
+    vim.api.nvim_create_user_command("ToggleMarkdownRender", function(args)
         require("render-markdown").toggle()
-      end
     end, { nargs = 1 })
   end,
 }
