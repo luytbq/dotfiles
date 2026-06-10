6615f43152f542eb62d7117f9ca69f284767428b
Bùi Quốc Luýt <luytbq@onepay.vn>
2026-06-10 09:10:01 +0700

    [cherry-picked] feat(nvim): render-markdown plugin command

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -6,6 +6,6 @@ return {
     require("render-markdown").setup(opts)
     vim.api.nvim_create_user_command("ToggleMarkdownRender", function(args)
         require("render-markdown").toggle()
-    end, { nargs = 1 })
+    end, {})
   end,
 }
